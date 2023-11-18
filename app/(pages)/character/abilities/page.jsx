89b6e0dddd0a98ae8b279f5386af4b03e921698b
Tony <tienloctonypham@gmail.com>
2023-11-18 06:41:29 -0500

    UX Redesign Css fixes

--- app/(pages)/character/abilities/page.jsx
+++ app/(pages)/character/abilities/page.jsx
@@ -4,7 +4,7 @@ import { specialAbilities } from "@/stats/misc/specialAbilities";
 export default function Page() {
   return (
     <div>
-      <div className="grid grid-cols-4 gap-3 p-5">
+      <div className="flex flex-wrap p-5 justify-between gap-y-5">
         {Object.entries(specialAbilities).map(([key, value]) => <AbilityCard key={key} ability={value}/> )}
       </div>
     </div>
